05261b99bd452f8e56920cae97925b228d43782c
hwiung <khw5233@gmail.com>
2024-12-02 22:14:46 +0900

    test

--- src/main/java/com/sparta/currency_user/dto/CurrencyRequestDto.java
+++ src/main/java/com/sparta/currency_user/dto/CurrencyRequestDto.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @Getter
 
 public class CurrencyRequestDto {
-    @NotNull(message = "currencyName 값은 필수입니다111111")
+    @NotNull(message = "currencyName 값은 필수입니다777")
     private String currencyName;
     @NotNull(message = "exchangeRate 값은 필수입니다")
     private BigDecimal exchangeRate;
